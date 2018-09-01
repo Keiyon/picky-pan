87c9d7ae83ed17ae3197b72833d9da7da89857a6
Keiyon Tyler <tookie@Keiyons-MBP.fios-router.home>
2018-09-01 19:42:14 -0400

    about me

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-$(document)
-=======
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
@@ -9,5 +6,5 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
-}
->>>>>>> 5b1e28a292c14c89c0e31e4dd880bab9a177bc86
+}unnies0513k
+
